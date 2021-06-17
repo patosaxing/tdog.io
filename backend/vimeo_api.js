4a95b20cd8f8d7b4264086e1217a551a51c3a44f
Osric <dhoubade@ualberta.ca>
2021-06-17 11:31:44 -0600

    Vimeo API working and moved it to different file

---
+++ backend/vimeo_api.js
@@ -0,0 +1,36 @@
+import { Vimeo } from 'vimeo';
+
+//  Vimeo API from the SDK
+
+let client = new Vimeo("c092d1b416b77b77fda26acd87bb603ec1f4be09", "5xYj7OU8U8bXQqmz4L5KsnugLn2TKSUwgXZMGeqyQQ1Ja1tpNbT6Ec4BfhkvON8cQklNNT8L0s0vk5TBWRyKhZC8SdTI/EuP4OSgBHzSvpiUN3crrrlhYThsgFgQtPjB", "5ff8513a276b8f1da5f1e046f352c644");
+
+// Checking to test Vimeo API: Note- was successful
+// client.request({
+//   method: 'GET',
+//   path: '/tutorial'
+// }, function (error, body, status_code, headers) {
+//   if (error) {
+//     console.log(error);
+//   }
+//   console.log(body);
+// })
+
+// Upload to Vimeo
+const file_name = "./uploads/Pexels.mp4"
+client.upload(
+  file_name,
+  {
+    'name': 'sample',
+    'description': 'The description goes here.'
+  },
+  function (uri) {
+    console.log('Your video yyyyy URI is: ' + uri);
+  },
+  function (bytes_uploaded, bytes_total) {
+    var percentage = (bytes_uploaded / bytes_total * 100).toFixed(2)
+    console.log(bytes_uploaded, bytes_total, percentage + '%')
+  },
+  function (error) {
+    console.log('Failed because: ' + error)
+  }
+);
