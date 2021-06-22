46cf0fb5df8deceb5f544933c993355741026a8f
Osric <dhoubade@ualberta.ca>
2021-06-22 14:28:51 -0600

    Just a comment to test

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,8 +1,11 @@
 import WebcamStreamCapture from "./Components/WebStreamCapture";
+import QuestionSelection from "./Components/Questions";
 // import Webcam from "react-webcam";
+//THis is a test
 function App() {
   return (
     <div className="App">
+      <QuestionSelection/>
       <WebcamStreamCapture/>
     </div>
   );
