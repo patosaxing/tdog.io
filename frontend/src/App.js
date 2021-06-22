c3733c286c63ba212fbc1f3ee74632a379416b2e
Osric <dhoubade@ualberta.ca>
2021-06-22 10:45:10 -0600

    Removed clutters

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,11 +1,8 @@
-//import RecordVideo from "./Components/RecordVideo";
 import WebcamStreamCapture from "./Components/WebStreamCapture";
 
 function App() {
   return (
     <div className="App">
-      {/* <Webcam audio="true" /> */}
-      {/* <button>Record</button> */}
       <WebcamStreamCapture/>
     </div>
   );
