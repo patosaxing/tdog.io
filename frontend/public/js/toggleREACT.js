f19ac4f830385c7a1fcfead7c634256df3551cae
ChuongTang <chuong.tang@aol.com>
2021-07-08 23:35:33 -0600

    added placeholder for webcam on/off

--- frontend/public/js/toggleREACT.js
+++ frontend/public/js/toggleREACT.js
@@ -1,8 +1,10 @@
 const showReact = ()=> {
   document.getElementById("reactSec").classList.remove("hideReact");
   document.getElementById("reactSec").classList.add("showReact");
+  // ask for webcam
 };
 const hideReact = ()=> {
   document.getElementById("reactSec").classList.remove("showReact");
   document.getElementById("reactSec").classList.add("hideReact");
+  // hide webcam "asking permission"
 }
