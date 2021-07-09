c9cd42d6f4710b5b279faab5f884c4bab1e400c9
Patrick <patrickosaje@gmail.com>
2021-07-08 20:57:15 -0600

    login-register working

--- backend/models/user.js
+++ backend/models/user.js
@@ -11,7 +11,8 @@ const UserSchema = new Schema(
             required: [true, "can't be blank"]
         },
 
-        password: { type: String, required: true, maxLength: 20 },
+        password: { type: String, required: true, maxLength: 1000 },
+        salt: { type: String, required: true, maxLength: 1000 },
         firstName: { type: String, required: true, trim: true, maxLength: 25},
         lastName: { type: String, required: true, trim: true, maxLength: 25},
         birthDate: { type: Date, required: false }
