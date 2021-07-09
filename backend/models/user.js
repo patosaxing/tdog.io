1b733cfc00234d52d8809ae16ba8317bebce0659
ChuongTang <chuong.tang@aol.com>
2021-07-09 11:32:53 -0600

    Revert "prior to connecting BE"
    This reverts commit e2a17252055728318d7841b1e91da81edbfac27e.

--- backend/models/user.js
+++ backend/models/user.js
@@ -5,25 +5,16 @@ const Schema = mongoose.Schema
 
 const UserSchema = new Schema(
     {
-        email: {
+        email:{
             type: String,
             lowercase: true,
-            unique: true,
-            required: [true, "can't be blank"],
-            match: [
-                /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/,
-                "Please provide a valid email",
-            ],
+            required: [true, "can't be blank"]
         },
 
-        password: { 
-            type: String, 
-            required: [true, "Please provide a password" ],
-            minLength: 6,
-            maxLength: 100, },
+        password: { type: String, required: true, maxLength: 1000 },
         salt: { type: String, required: true, maxLength: 1000 },
-        firstName: { type: String, required: true, trim: true, maxLength: 25 },
-        lastName: { type: String, required: true, trim: true, maxLength: 25 },
+        firstName: { type: String, required: true, trim: true, maxLength: 25},
+        lastName: { type: String, required: true, trim: true, maxLength: 25},
         birthDate: { type: Date, required: false }
     }
 )
