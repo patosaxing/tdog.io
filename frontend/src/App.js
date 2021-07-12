34e9b19a9a486cde8dd6529200317d34695a8e6c
chuongtang <chuong.tang@aol.com>
2021-07-11 23:38:20 -0600

    Added NavBar for rebulding from scratch

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,18 +1,19 @@
 import WebcamStreamCapture from "./Components/WebStreamCapture.jsx";
-import AppBar from "./Components/AppBar.jsx";
-// import VideosGrid from "./Components/VideosGrid.jsx";
+// import AppBar from "./Components/AppBar.jsx";
+import VideosGrid from "./Components/VideosGrid.jsx";
 import QuestionSelection from "./Components/Questions";
-
+import NavBar from "./Components/NavBar.jsx";
 
 
 function App() {
   return (
     <div className="App">
-      <AppBar />
-      <QuestionSelection/>
+      {/* <AppBar /> */}
+      <NavBar />
+      <QuestionSelection />
       <WebcamStreamCapture />
-      {/* <VideosGrid /> */}
+      <VideosGrid />
     </div>
   );
-  }
+}
 export default App;
---
+++ frontend/src/Components/NavBar.jsx
@@ -0,0 +1,82 @@
+import React, { useState } from "react";
+import { makeStyles } from "@material-ui/core/styles";
+import AppBar from "@material-ui/core/AppBar";
+import Toolbar from "@material-ui/core/Toolbar";
+import Typography from "@material-ui/core/Typography";
+import IconButton from "@material-ui/core/IconButton";
+import MenuIcon from "@material-ui/icons/Menu";
+import green from "@material-ui/core/colors/green";
+
+// react.school/material-ui
+
+const useStyles = makeStyles((theme) => ({
+  menuButton: {
+    marginRight: theme.spacing(2)
+  },
+  title: {
+    flexGrow: 1
+  },
+  customColor: {
+    // or hex code, this is normal CSS background-color
+    backgroundColor: green[500]
+  },
+  customHeight: {
+    minHeight: 200
+  },
+  offset: theme.mixins.toolbar
+}));
+
+export default function ButtonAppBar() {
+  const classes = useStyles();
+  return (
+    <React.Fragment>
+      <AppBar
+        color={'default'}
+        className={'default 16'}
+      >
+        <Toolbar>
+          <IconButton
+            edge="start"
+            className={classes.menuButton}
+            color="inherit"
+            aria-label="menu"
+          >
+            <MenuIcon />
+          </IconButton>
+          <Typography variant="h2" className={classes.title}>
+            Eval-view 
+          </Typography>
+          <IconButton color="inherit" >
+            Feature 
+          </IconButton>
+          <IconButton color="inherit" >
+            About 
+          </IconButton>
+          <IconButton color="inherit" >
+            Service 
+          </IconButton>
+          <IconButton color="inherit" >
+            Team
+          </IconButton>
+          <IconButton color="inherit" >
+            Contact
+          </IconButton>
+          <IconButton color="inherit" >
+            LOGIN / SIGN-UP
+          </IconButton>
+        </Toolbar>
+      </AppBar>
+      <Toolbar />
+      <Typography>
+        This content doesn't appear below the AppBar because we added an
+        additional Toolbar component above, this is the recommended approach.{" "}
+      </Typography>
+      <Typography>
+        Change the AppBar example by clicking on one of the numbers above.
+      </Typography>
+      <Typography>
+      some text here
+      </Typography>
+    </React.Fragment>
+  );
+}
