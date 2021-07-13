4ca36672dbe0ed37a6c5b740b4dd3070f6eeb15c
chuongtang <chuong.tang@aol.com>
2021-07-12 21:50:23 -0600

    added upload folder at frontend

--- backend/index.js
+++ backend/index.js
@@ -1,5 +1,6 @@
 //Express and Cors import statement
 const express = require('express');
+const dotenv = require('dotenv');
 const helmet = require("helmet");
 const compression = require('compression');
 const colors = require('colors'); // customise consoleLog with colors for easy debugging
@@ -9,8 +10,8 @@ app.use(compression());
 const cors = require('cors');
 const cookieParser = require('cookie-parser');
 const connectDB = require("./config/db"); // move db to config for expanding model
+const fileUpload = require('express-fileupload'); // upload file to server
 
-const dotenv = require('dotenv');
 
 const userRoutes = require("./routes/userRoutes")
 
@@ -26,11 +27,31 @@ app.use(express.json())
 app.use(cookieParser())
 app.use(express.urlencoded({ extended: false }))
 
+app.use(fileUpload());
+
 app.use('/', userRoutes)
 
 app.get('/api', (req, res, next) => {
   res.send("TDOG Api is running"); // TESTED -> WORKED
   console.log('this is root'.green.bold);
+});
+
+// Upload endpoint
+app.post('/api/upload', (req, res) => {
+  if (req.files === null) {
+    return res.status(400).json({ msg: 'No file uploaded' });
+  }
+
+  const file = req.files.file;
+
+  file.mv(`${__dirname}/frontend/public/uploads/${file.name}`, err => {
+    if (err) {
+      console.error(err);
+      return res.status(500).send(err);
+    }
+
+    res.json({ fileName: file.name, filePath: `/uploads/${file.name}` });
+  });
 });
 
 //Listening Port
