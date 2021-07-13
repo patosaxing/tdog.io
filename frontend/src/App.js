dbfc1d77d1f33b0cd15f6199ac9a38c522796f4e
chuongtang <chuong.tang@aol.com>
2021-07-12 23:03:04 -0600

    Tested FileUploader -> WORKED!

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -2,6 +2,7 @@ import WebcamStreamCapture from "./Components/WebStreamCapture.jsx";
 import AppBar from "./Components/AppBar.jsx";
 import VideosGrid from "./Components/VideosGrid.jsx";
 import QuestionSelection from "./Components/Questions";
+import FileUpload from './Components/FileUpload/FileUploader';
 
 
 function App() {
@@ -10,6 +11,7 @@ function App() {
       <AppBar />
       <QuestionSelection />
       <WebcamStreamCapture />
+      <FileUpload />
       <VideosGrid />
     </div>
   );
