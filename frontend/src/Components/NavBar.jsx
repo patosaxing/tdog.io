520509577544de3b2fff9e67b83a7a8295795fde
chuongtang <chuong.tang@aol.com>
2021-07-12 18:16:37 -0600

    removing landing page parts

--- frontend/src/Components/NavBar.jsx
+++ frontend/src/Components/NavBar.jsx
@@ -4,26 +4,28 @@ import AppBar from "@material-ui/core/AppBar";
 import Toolbar from "@material-ui/core/Toolbar";
 import Typography from "@material-ui/core/Typography";
 import IconButton from "@material-ui/core/IconButton";
-import MenuIcon from "@material-ui/icons/Menu";
-import green from "@material-ui/core/colors/green";
+import Logo from "../eval_view_logo.jpg";
+
+// import MenuIcon from "../";
+// import green from "@material-ui/core/colors/green";
 
 // react.school/material-ui
 
 const useStyles = makeStyles((theme) => ({
   menuButton: {
-    marginRight: theme.spacing(2)
+    marginRight: theme.spacing(2),
   },
   title: {
-    flexGrow: 1
+    flexGrow: 1,
   },
-  customColor: {
-    // or hex code, this is normal CSS background-color
-    backgroundColor: green[500]
-  },
-  customHeight: {
-    minHeight: 200
-  },
-  offset: theme.mixins.toolbar
+  // customColor: {
+  //   // or hex code, this is normal CSS background-color
+  //   backgroundColor: green[500]
+  // },
+  // customHeight: {
+  //   minHeight: 200
+  // },
+  offset: theme.mixins.toolbar,
 }));
 
 export default function ButtonAppBar() {
@@ -31,8 +33,8 @@ export default function ButtonAppBar() {
   return (
     <React.Fragment>
       <AppBar
-        color={'default'}
-        className={'default 16'}
+        color={"default"}
+        // className={'default 16'}
       >
         <Toolbar>
           <IconButton
@@ -41,29 +43,18 @@ export default function ButtonAppBar() {
             color="inherit"
             aria-label="menu"
           >
-            <MenuIcon />
+            <img src={Logo} alt="Logo" />
+            {/* <MenuIcon /> */}
           </IconButton>
           <Typography variant="h2" className={classes.title}>
-            Eval-view 
+            Eval-view
           </Typography>
-          <IconButton color="inherit" >
-            Feature 
-          </IconButton>
-          <IconButton color="inherit" >
-            About 
-          </IconButton>
-          <IconButton color="inherit" >
-            Service 
-          </IconButton>
-          <IconButton color="inherit" >
-            Team
-          </IconButton>
-          <IconButton color="inherit" >
-            Contact
-          </IconButton>
-          <IconButton color="inherit" >
-            LOGIN / SIGN-UP
-          </IconButton>
+          <IconButton color="inherit">Feature</IconButton>
+          <IconButton color="inherit">About</IconButton>
+          <IconButton color="inherit">Service</IconButton>
+          <IconButton color="inherit">Team</IconButton>
+          <IconButton color="inherit">Contact</IconButton>
+          <IconButton color="inherit">LOGIN / SIGN-UP</IconButton>
         </Toolbar>
       </AppBar>
       <Toolbar />
@@ -74,9 +65,7 @@ export default function ButtonAppBar() {
       <Typography>
         Change the AppBar example by clicking on one of the numbers above.
       </Typography>
-      <Typography>
-      some text here
-      </Typography>
+      <Typography>some text here</Typography>
     </React.Fragment>
   );
 }
