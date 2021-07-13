9f61d7fe043e17e8521f5852781a75f55da1d2b9
chuongtang <chuong.tang@aol.com>
2021-07-12 18:22:23 -0600

    swap NavBar with AppBar

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,15 +1,15 @@
 import WebcamStreamCapture from "./Components/WebStreamCapture.jsx";
-// import AppBar from "./Components/AppBar.jsx";
+import AppBar from "./Components/AppBar.jsx";
 import VideosGrid from "./Components/VideosGrid.jsx";
 import QuestionSelection from "./Components/Questions";
-import NavBar from "./Components/NavBar.jsx";
+// import NavBar from "./Components/NavBar.jsx";
 
 
 function App() {
   return (
     <div className="App">
-      {/* <AppBar /> */}
-      <NavBar />
+      <AppBar />
+      {/* <NavBar /> */}
       <QuestionSelection />
       <WebcamStreamCapture />
       <VideosGrid />
