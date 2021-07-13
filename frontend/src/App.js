3dafe413ecddfcbdae35b02eb4b3ae467619944e
chuongtang <chuong.tang@aol.com>
2021-07-12 21:55:56 -0600

    added proxy in frontend package

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -2,14 +2,12 @@ import WebcamStreamCapture from "./Components/WebStreamCapture.jsx";
 import AppBar from "./Components/AppBar.jsx";
 import VideosGrid from "./Components/VideosGrid.jsx";
 import QuestionSelection from "./Components/Questions";
-// import NavBar from "./Components/NavBar.jsx";
 
 
 function App() {
   return (
     <div className="App">
       <AppBar />
-      {/* <NavBar /> */}
       <QuestionSelection />
       <WebcamStreamCapture />
       <VideosGrid />
