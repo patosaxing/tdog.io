1d7f5d753d5da309544f6584687e5c2ab2e93ec5
chuongtang <chuong.tang@aol.com>
2021-07-17 14:25:20 -0600

    added screens and actions

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,19 +1,34 @@
 import WebcamStreamCapture from "./Components/WebStreamCapture.jsx";
-import AppBar from "./Components/AppBar.jsx";
+// import AppBar from "./Components/AppBar.jsx";
+import Header from "./Components/Header.jsx";
+import Footer from "./Components/Footer.jsx"
 import VideosGrid from "./Components/VideosGrid.jsx";
 import QuestionSelection from "./Components/Questions";
 import FileUpload from './Components/FileUpload/FileUploader';
+import {Container} from 'react-bootstrap';
+import { Router } from "react-router-dom";
+import LoginScreen from './screens/LoginScreen';
+import RegisterScreen from './screens/RegisterScreen';
 
 
 function App() {
   return (
-    <div className="App">
-      <AppBar />
-      <QuestionSelection />
-      <WebcamStreamCapture />
-      <FileUpload />
-      <VideosGrid />
-    </div>
+    <Router>
+      <Header />
+      <main className="py-3">
+        <Container>
+          <Route path='/login' component={LoginScreen} />
+          <Route path='/register' component={RegisterScreen} />
+          <QuestionSelection />
+        <WebcamStreamCapture />
+        <FileUpload />
+        <VideosGrid />
+        </Container>
+      </main>
+      <Footer />
+    </Router>
+     
+      
   );
 }
 export default App;
