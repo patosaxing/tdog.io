6ae128cd35fbef2e6593ea31ca110cff407bea40
chuongtang <chuong.tang@aol.com>
2021-07-19 21:21:11 -0600

    Completed toggle components for loggED-in users

--- frontend/src/screens/PrivateScreen.jsx
+++ frontend/src/screens/PrivateScreen.jsx
@@ -1,5 +1,5 @@
 import React, { useState } from "react";
-import { Row, Col } from "react-bootstrap";
+import { Row, Col, Alert } from "react-bootstrap";
 import Video from "../components/Video";
 import videos from "../PlaceHolderData/Videos";
 import QuestionSelection from "../components/Questions";
@@ -10,23 +10,20 @@ const PrivateScreen = () => {
   const [loggedIN, setLoggedIN] = useState(true);
   return (
     <div>
-      {loggedIN ? (
-        <div>
-          <QuestionSelection />
-          <WebcamStreamCapture />
-          <FileUpload />
-          <h1>Your uploaded videos </h1>
-          <Row>
-            {videos.map((video) => (
-              <Col key={video._id} sm={12} md={6} lg={4} xl={3}>
-                <Video video={video} />
-              </Col>
-            ))}
-          </Row>
-        </div>
-      ) : (
-        <h7 style={{backgroundColor:'lightblue'}}> User has NOT logged in</h7>
-      )}
+      <div>
+      <Alert variant="info" style={{float: 'right'}}>Welcome: ..'userLogin.state'..</Alert>
+        <QuestionSelection />
+        <WebcamStreamCapture />
+        <FileUpload />
+        <h1>Your uploaded videos </h1>
+        <Row>
+          {videos.map((video) => (
+            <Col key={video._id} sm={12} md={6} lg={4} xl={3}>
+              <Video video={video} />
+            </Col>
+          ))}
+        </Row>
+      </div>
     </div>
   );
 };
