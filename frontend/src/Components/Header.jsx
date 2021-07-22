3376746754a82b04d6828a0ed14c1292b1302140
chuongtang <chuong.tang@aol.com>
2021-07-22 05:41:35 -0600

    ready for testing

--- frontend/src/Components/Header.jsx
+++ frontend/src/Components/Header.jsx
@@ -5,7 +5,9 @@ import { LinkContainer } from "react-router-bootstrap";
 import { Navbar, Nav, Container, NavDropdown } from "react-bootstrap";
 import SearchBox from "./SearchBox";
 import { logout } from "../actions/userActions";
-import Logo from './ReactSecLogo.png';
+import Logo from "./ReactSecLogo.png";
+import Exit from "../img/Exit.svg";
+import Login from "../img/Login.svg"
 
 const Header = () => {
   const dispatch = useDispatch();
@@ -22,23 +24,29 @@ const Header = () => {
       <Navbar bg="dark" variant="dark" expand="lg" collapseOnSelect>
         <Container>
           <LinkContainer to="/">
-            <Navbar.Brand> <img style={{maxWidth: "2.75rem"}} src={Logo} alt="Logo" /> Eval-view</Navbar.Brand>
+            <Navbar.Brand>
+              {" "}
+              <img style={{ maxWidth: "2.75rem" }} src={Logo} alt="Logo" />{" "}
+              Eval-view
+            </Navbar.Brand>
           </LinkContainer>
           <h6 style={{ color: "transparent" }}>
-              Adjusting NavBar spacer to Search Box
-            </h6>
+            Adjusting NavBar spacer to Search Box
+          </h6>
           <Navbar.Toggle aria-controls="basic-navbar-nav" />
           <Navbar.Collapse id="basic-navbar-nav">
             <Route render={({ history }) => <SearchBox history={history} />} />
-            <h4 style={{ color: "transparent" }}>
-              spacer
-            </h4>
+            <h4 style={{ color: "transparent" }}>spacer</h4>
             <Nav className="ml-auto">
-            {/* {userInfo && (
+              {/* {userInfo && (
               <LinkContainer to="/VideoList">
                 <Nav.Link>🎞️VideoList</Nav.Link>
               </LinkContainer>)} */}
-               <h2>🧑‍🤝‍🧑</h2>
+              <img
+                style={{ maxWidth: "3.5rem" }}
+                src={Login}
+                alt="Logout-icon"
+              />{" "}
               {userInfo ? (
                 <NavDropdown title={userInfo.username} id="username">
                   <LinkContainer to="/profile">
@@ -65,6 +73,17 @@ const Header = () => {
               )}
             </Nav>
           </Navbar.Collapse>
+          <Navbar.Text>
+            <a href="https://gwhagerty.github.io/bootstrapp_eval_view/">
+              {" "}
+              <img
+                style={{ maxWidth: "3.5rem" }}
+                src={Exit}
+                alt="Logout-icon"
+              />{" "}
+              Exit the App
+            </a>
+          </Navbar.Text>
         </Container>
       </Navbar>
     </header>
