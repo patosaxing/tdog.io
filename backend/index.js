73cc3c3955d4097820115476cb2c68925079059f
chuongtang <chuong.tang@aol.com>
2021-08-09 18:18:02 -0600

    added PRocfile and commit for deployment

--- backend/index.js
+++ backend/index.js
@@ -15,8 +15,8 @@ const videoRoutes = require("./routes/videoRoutes");
 
 //Middleware
 app.use(cors());
-app.use(express.json({limit: '20mb', extended: true}));  // limit file size for profile img upload
-app.use(express.urlencoded({limit: '20mb', extended: true}));  // limit file size for profile img upload
+app.use(express.json({ limit: '20mb', extended: true }));  // limit file size for profile img upload
+app.use(express.urlencoded({ limit: '20mb', extended: true }));  // limit file size for profile img upload
 app.use(helmet()); // routes protection
 app.use(compression());
 app.use(fileUpload());
@@ -29,9 +29,22 @@ app.use('/api/users', userRoutes);
 app.use('/api/videos', videoRoutes);
 
 
+
+// const __dirname = path.resolve();
+// app.use('/uploads', express.static(path.join(__dirname, '/uploads')));
+
+if (process.env.NODE_ENV === 'production') {
+  app.use(express.static(path.join(__dirname, '/frontend/build')));
+  app.get('*', (req, res) =>
+    res.sendFile(path.resolve(__dirname, 'frontend', 'build', 'index.html'))
+  );
+} else {
+  app.get('/', (req, res) => {
+    res.send('App is running at ROOT');
+  });
+}
 //Port Connection
 const port = process.env.PORT || 5000
-
 
 // Listening Port
 app.listen(port, () => {
