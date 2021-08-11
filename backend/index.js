daa6097db6bc662c95e1cf904234a67eb8931ee9
chuongtang <chuong.tang@aol.com>
2021-08-10 22:22:47 -0600

    add sirnam

--- backend/index.js
+++ backend/index.js
@@ -35,8 +35,8 @@ app.use('/api/videos', videoRoutes);
 // app.use('/uploads', express.static(path.join(__dirname, '/uploads')));
 
 if (process.env.NODE_ENV === 'production') {
-  app.use(express.static('../frontend/build'));
-  // app.use(express.static(path.join(__dirname, 'build')));
+  // app.use(express.static('../frontend/build'));
+  app.use(express.static(path.join(__dirname, './frontend/build')));
   app.get('*', (req, res) => {
     res.sendFile(path.resolve(__dirname, 'frontend', 'build', 'index.html'))
     // const index = path.join(__dirname, 'build','index.html');
