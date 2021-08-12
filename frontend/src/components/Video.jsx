62db6e373946e7e874750bc71bffdb050aa90355
chuongtang <chuong.tang@aol.com>
2021-08-12 16:11:23 -0600

    successfully displace publiVideo cards at first loggedIN

--- frontend/src/components/Video.jsx
+++ frontend/src/components/Video.jsx
@@ -6,28 +6,28 @@ import VideoUpload from "../img/VideoUpload.svg";
 
 const Video = ({ video }) => {
   return (
-    <Card className="my-3 p-3 rounded">
-      
+    <Card className="my-3 p-3">
       {/* <a href={`/video/${video._id}`}>
         <Card.Img src={video.image} variant='top' />
       </a> */}
 
       <Card.Body>
-        <a href={`/video/${video._id}`}>
+        
           <Card.Title as="div">
-            <strong>{video.name}</strong>
+            <strong>{video.category}</strong>
           </Card.Title>
+          <Card.Subtitle className="mb-2 text-muted">{video.description}</Card.Subtitle>
+        
+        <a href={video.videoLink}>
+          {/* <a href={`/video/${video._id}`}> */}
+          <div style={{ width: 66, height: "auto" }}>
+            <Card.Img src={VideoUpload} />
+          </div>
         </a>
-        <a href={`/video/${video._id}`}>
-        <div style={{ width: 66, height: "auto" }}>
-          
-        <Card.Img src={VideoUpload} />
-         
-        </div>
-      </a>
         <Card.Text as="div">
           Rating:
           <Rating value={video.rating} text={``} />
+          {/* <Rating value={video.rating} text={``} /> */}
         </Card.Text>
 
         <Card.Text as="h6">Total {video.numReviews} reviews</Card.Text>
