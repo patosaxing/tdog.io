74386747e4f1ff6005e91d381347296cb9b1abd8
chuongtang <chuong.tang@aol.com>
2021-08-14 16:26:41 -0600

    added video preview on Modal

--- frontend/src/components/Video.jsx
+++ frontend/src/components/Video.jsx
@@ -3,6 +3,7 @@ import { Card, Modal, Button, Offcanvas } from "react-bootstrap";
 // import SampleVideo from './R.png'
 import Rating from "./Rating";
 import VideoUpload from "../img/VideoUpload.svg";
+import PreviewVideo from "./PreviewVideo";
 
 const Video = ({ video }) => {
   const [show, setShow] = useState(false);
@@ -10,14 +11,16 @@ const Video = ({ video }) => {
   const handleClose = () => setShow(false);
   const handleShow = () => setShow(true);
   // const [isOpen, setOpen] = useState(false);
+  const newVideoLink = `${video.videoLink}`.replace(
+    "/view?usp=drivesdk",
+    "/preview"
+  );
+  console.log(newVideoLink);
+  
 
   return (
     <Card className="my-3 p-3">
-      {/* <a href={`/video/${video._id}`}>
-        <Card.Img src={video.image} variant='top' />
-      </a> */}
-
-      <Card.Body>
+          <Card.Body>
         <Card.Title as="div">
           <strong>{video.user}</strong>
         </Card.Title>
@@ -31,13 +34,19 @@ const Video = ({ video }) => {
 
         <Offcanvas show={show} onHide={handleClose}>
           <Offcanvas.Header closeButton>
-            <Offcanvas.Title>{video.user}</Offcanvas.Title>
+            <Offcanvas.Title>{video.videoLink}</Offcanvas.Title>
           </Offcanvas.Header>
           <Offcanvas.Body>
-            <video width="320" height="240" controls>
-              <source src={video.videoLink} type="video/webm"></source>
-              Your browser does not support the video tag.
-            </video>
+          
+            <iframe
+              src={newVideoLink}
+              type="video/webm"
+              width="100%"
+              height="auto"
+              autoplay
+              allowFullScreen
+            ></iframe>
+            
           </Offcanvas.Body>
         </Offcanvas>
         {/* Offcanvas End */}
