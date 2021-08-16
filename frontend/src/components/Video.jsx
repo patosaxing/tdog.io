b5958e063e760f3b97f887562d12b4106e87128d
chuongtang <chuong.tang@aol.com>
2021-08-16 13:36:40 -0600

    added Redux state for comment component

--- frontend/src/components/Video.jsx
+++ frontend/src/components/Video.jsx
@@ -1,7 +1,17 @@
 import React, { useState } from "react";
-import { Card, Modal, Button, Offcanvas } from "react-bootstrap";
-// import SampleVideo from './R.png'
+import {
+  Card,
+  Modal,
+  Button,
+  Offcanvas,
+  Col,
+  Form,
+  ListGroup,
+} from "react-bootstrap";
+import { useDispatch, useSelector } from "react-redux";
+import { Link } from "react-router-dom";
 import Rating from "./Rating";
+import Message from "../components/Message";
 
 const Video = ({ video }) => {
   const [show, setShow] = useState(false);
@@ -15,9 +25,29 @@ const Video = ({ video }) => {
   );
   // console.log(newVideoLink);
 
-  const handleLike = (video) => {
-    
+  const dispatch = useDispatch();
+
+  const userLogin = useSelector((state) => state.userLogin);
+  const { userInfo } = userLogin;
+
+  const videoReviewCreate = useSelector((state) => state.videoReviewCreate);
+  const { success: successVideoReview, error: errorVideoReview, } = videoReviewCreate;
+
+  const [comment, setComment] = useState("");
+  const [rating, setRating] = useState(3.5);
+
+
+  const submitHandler = (e) => {
+    e.preventDefault();
+    dispatch(
+      createVideoReview(match.params.id, {
+        rating,
+        comment,
+      })
+    );
   }
+
+  const handleLike = (video) => {};
 
   return (
     <Card className="my-3 p-3">
@@ -25,6 +55,7 @@ const Video = ({ video }) => {
         <Card.Title as="div">
           <strong>{video.user}</strong>
         </Card.Title>
+
         <Card.Subtitle className="mb-2 text-muted">
           {video.description}
         </Card.Subtitle>
@@ -47,9 +78,68 @@ const Video = ({ video }) => {
               allowFullScreen
             ></iframe>
             <h2>User: {video.user}</h2>
-            <Rating value={video.rating} />
+            <Rating value={video.rating} text={`${video.numReviews} reviews`} />
             <h6>Category: {video.category}</h6>
             <h6>Description: {video.description}</h6>
+            <Col md={6}>
+              <h2>Reviews</h2>
+              {video.reviews.length === 0 && <Message>No Reviews</Message>}
+              <ListGroup variant="flush">
+                {video.reviews.map((review) => (
+                  <ListGroup.Item key={review._id}>
+                    <strong>{review.name}</strong>
+                    <Rating value={review.rating} />
+                    <p>{review.createdAt.substring(0, 10)}</p>
+                    <p>{review.comment}</p>
+                  </ListGroup.Item>
+                ))}
+                <ListGroup.Item>
+                  <h2>Write a Customer Review</h2>
+                  {errorVideoReview && (
+                    <Message variant="danger">{errorVideoReview}</Message>
+                  )}
+                  {userInfo ? (
+                    <Form onSubmit={submitHandler}>
+                      <Form.Group controlId="rating">
+                        <Form.Label>Rating</Form.Label>
+                        <Form.Control
+                          as="select"
+                          value={rating}
+                          onChange={(e) => setRating(e.target.value)}
+                        >
+                          <option value="">Select...</option>
+                          <option value="1">1 - Very Poor</option>
+                          <option value="1.5">1.5 - Poor</option>
+                          <option value="2">2 - Fair</option>
+                          <option value="2.5">2.5 - Average</option>
+                          <option value="3">3 - Above Average</option>
+                          <option value="3.5">3.5 - Good</option>
+                          <option value="4">4 - Very Good</option>
+                          <option value="4.5">4.5 - Great</option>
+                          <option value="5">5 - Excellent🎉</option>
+                        </Form.Control>
+                      </Form.Group>
+                      <Form.Group controlId="comment">
+                        <Form.Label>Comment</Form.Label>
+                        <Form.Control
+                          as="textarea"
+                          row="3"
+                          value={comment}
+                          onChange={(e) => setComment(e.target.value)}
+                        ></Form.Control>
+                      </Form.Group>
+                      <Button type="submit" variant="primary">
+                        Submit
+                      </Button>
+                    </Form>
+                  ) : (
+                    <Message>
+                      Please <Link to="/login">sign in</Link> to write a review{" "}
+                    </Message>
+                  )}
+                </ListGroup.Item>
+              </ListGroup>
+            </Col>
           </Offcanvas.Body>
         </Offcanvas>
         {/* Offcanvas End */}
