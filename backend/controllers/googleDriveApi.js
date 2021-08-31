ce00a26ebba3a62bedfc6d48c7ed266bf2c91fbc
chuongtang <chuong.tang@aol.com>
2021-08-31 13:58:31 -0600

    prior to adding DeleteFileOnG

--- backend/controllers/googleDriveApi.js
+++ backend/controllers/googleDriveApi.js
@@ -67,8 +67,7 @@ exports.uploadToG = async (fileN, uploadUser, qCat, qSkill) => {
 
     // console.log('resutl from ext URL function'.magenta, result.data.webViewLink);
     return { id: response.data.id, url: result.data.webViewLink }
-    // // Save video metadata to MongoDB
-    // videoControl.videoDetailToMongo(fileId, result.data.webViewLink, uploadUser, qCat, qSkill);
+    
 
   } catch (error) {
     console.log('error from google Drive API: ', error.message);
