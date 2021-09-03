d3ba203d988a9236a5636d35520d1bbdc7bd81f5
chuongtang <chuong.tang@aol.com>
2021-09-02 22:31:38 -0600

    fixed the live URL for resetTOken

--- backend/controllers/authControl.js
+++ backend/controllers/authControl.js
@@ -83,7 +83,7 @@ const authControl = {
             await user.save();
 
             // Create reset url to email to provided email-address
-            const resetUrl = `http://localhost:3000/passwordreset/${resetToken}`;
+            const resetUrl = `https://evalview.herokuapp.com/passwordreset/${resetToken}`;
 
             // Message sent as an HTML body
             const message = `
